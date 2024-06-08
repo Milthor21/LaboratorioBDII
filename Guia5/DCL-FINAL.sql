bc86a3cef7e20037c385a1b264af54c7d2077f4e
Milton Cuadra <128653541+Milthor21@users.noreply.github.com>
2024-06-08 13:02:09 -0600

    actualizacion
    actualizacion

--- Guia5/DCL-FINAL.sql
+++ Guia5/DCL-FINAL.sql
@@ -13,96 +13,6 @@ create role if not exists 'Ingeniero';
 create role if not exists 'Planificador';
 create role if not exists 'Cliente';
 
--- Asignacion de privilegios a roles
-
--- Admin
-grant all on aerolinea.* to 'SysAdmin';
-
--- Soporte
-grant select, insert, update, delete on aerolinea.asiento to Soporte;
-grant select, insert, update, delete on aerolinea.boleto to Soporte;
-grant select, insert, update, delete on aerolinea.ciudad to Soporte;
-grant select, insert, update, delete on aerolinea.clases to Soporte;
-grant select, insert, update, delete on aerolinea.destino to Soporte;
-grant select, insert, update, delete on aerolinea.direcciones to Soporte;
-grant select, insert, update, delete on aerolinea.estado to Soporte;
-grant select, insert, update, delete on aerolinea.fecha to Soporte;
-grant select, insert, update, delete on aerolinea.itinerario to Soporte;
-grant select, insert, update, delete on aerolinea.metodopago to Soporte;
-grant select, insert, update, delete on aerolinea.millas to Soporte;
-grant select, insert, update, delete on aerolinea.origen to Soporte;
-grant select, insert, update, delete on aerolinea.pago to Soporte;
-grant select, insert, update, delete on aerolinea.pais to Soporte;
-grant select, insert, update, delete on aerolinea.pasajero to Soporte;
-grant select, insert, update, delete on aerolinea.reservacion to Soporte;
-grant select, insert, update, delete on aerolinea.salida to Soporte;
-grant select, insert, update, delete on aerolinea.tarifa to Soporte;
-grant select, insert, update, delete on aerolinea.tipoequipaje to Soporte;
-grant select, insert, update, delete on aerolinea.visa to Soporte;
-grant select, insert, update, delete on aerolinea.vuelo to Soporte;
-grant select, insert, update, delete on aerolinea.usuariospasajero to Soporte;
-grant select, insert, update, delete on aerolinea.usuarios to Soporte;
-
--- contabilidad
-grant select, insert, update, delete on aerolinea.metodopago to Contabilidad;
-grant select, insert, update, delete on aerolinea.millas to Contabilidad;
-grant select, insert, update, delete on aerolinea.pago to Contabilidad;
-grant select, insert, update, delete on aerolinea.tarifa to Contabilidad;
-
--- Inventario
-grant select, insert, update, delete on aerolinea.equipaje to Inventario;
-grant select, insert, update, delete on aerolinea.aeronave to Inventario;
-
--- RRHH
-grant select, insert, update, delete on aerolinea.cargo to RRHH;
-grant select, insert, update, delete on aerolinea.ciudad to RRHH;
-grant select, insert, update, delete on aerolinea.direcciones to RRHH;
-grant select, insert, update, delete on aerolinea.empleado to RRHH;
-grant select, insert, update, delete on aerolinea.estado to RRHH;
-grant select, insert, update, delete on aerolinea.pais to RRHH;
-grant select, insert, update, delete on aerolinea.piloto to RRHH;
-grant select, insert, update, delete on aerolinea.sobrecargo to RRHH;
-grant select, insert, update, delete on aerolinea.visa to RRHH;
-
--- Ingeniero
-grant select, insert, update, delete on aerolinea.clases to Ingeniero;
-grant select, insert, update, delete on aerolinea.aeronave to Ingeniero;
-
--- Planificador
-grant select, insert, update, delete on aerolinea.ciudad to Planificador;
-grant select, insert, update, delete on aerolinea.destino to Planificador;
-grant select, insert, update, delete on aerolinea.direcciones to Planificador;
-grant select, insert, update, delete on aerolinea.escala to Planificador;
-grant select, insert, update, delete on aerolinea.estado to Planificador;
-grant select, insert, update, delete on aerolinea.origen to Planificador;
-grant select, insert, update, delete on aerolinea.pais to Planificador;
-grant select, insert, update, delete on aerolinea.regreso to Planificador;
-grant select, insert, update, delete on aerolinea.salida to Planificador;
-grant select, insert, update, delete on aerolinea.vuelo to Planificador;
-
--- cliente
-grant select, insert, update, delete on aerolinea.asiento to Cliente;
-grant select, insert, update, delete on aerolinea.boleto to Cliente;
-grant select, insert, update, delete on aerolinea.ciudad to Cliente;
-grant select, insert, update, delete on aerolinea.clases to Cliente;
-grant select, insert, update, delete on aerolinea.destino to Cliente;
-grant select, insert, update, delete on aerolinea.direcciones to Cliente;
-grant select, insert, update, delete on aerolinea.estado to Cliente;
-grant select, insert, update, delete on aerolinea.fecha to Cliente;
-grant select, insert, update, delete on aerolinea.itinerario to Cliente;
-grant select, insert, update, delete on aerolinea.metodopago to Cliente;
-grant select, insert, update, delete on aerolinea.millas to Cliente;
-grant select, insert, update, delete on aerolinea.origen to Cliente;
-grant select, insert, update, delete on aerolinea.pago to Cliente;
-grant select, insert, update, delete on aerolinea.pais to Cliente;
-grant select, insert, update, delete on aerolinea.pasajero to Cliente;
-grant select, insert, update, delete on aerolinea.reservacion to Cliente;
-grant select, insert, update, delete on aerolinea.salida to Cliente;
-grant select, insert, update, delete on aerolinea.tarifa to Cliente;
-grant select, insert, update, delete on aerolinea.tipoequipaje to Cliente;
-grant select, insert, update, delete on aerolinea.visa to Cliente;
-grant select, insert, update, delete on aerolinea.vuelo to Cliente;
-grant select, insert, update, delete on aerolinea.usuariospasajero to Cliente;
 
 -- CREACION DE USUARIOS
 
@@ -156,6 +66,98 @@ password expire interval 180 day;
 create user if not exists 'Samuel_Flor'@'localhost' 
 identified with sha256_password by '1234' 
 password expire interval 180 day;
+
+-- Asignacion de privilegios a roles
+
+-- Admin
+grant all on aerolinea.* to 'Admin_Gil'@'localhost' ;
+
+-- Soporte
+grant select, insert, update, delete on aerolinea.asiento to 'Sop_gil'@'localhost' ;
+grant select, insert, update, delete on aerolinea.boleto to 'Sop_gil'@'localhost' ;
+grant select, insert, update, delete on aerolinea.ciudad to 'Sop_gil'@'localhost' ;
+grant select, insert, update, delete on aerolinea.clases to 'Sop_gil'@'localhost' ;
+grant select, insert, update, delete on aerolinea.destino to 'Sop_gil'@'localhost' ;
+grant select, insert, update, delete on aerolinea.direcciones to 'Sop_gil'@'localhost' ;
+grant select, insert, update, delete on aerolinea.estado to 'Sop_gil'@'localhost' ;
+grant select, insert, update, delete on aerolinea.fecha to 'Sop_gil'@'localhost' ;
+grant select, insert, update, delete on aerolinea.itinerario to 'Sop_gil'@'localhost' ;
+grant select, insert, update, delete on aerolinea.metodopago to 'Sop_gil'@'localhost' ;
+grant select, insert, update, delete on aerolinea.millas to 'Sop_gil'@'localhost' ;
+grant select, insert, update, delete on aerolinea.origen to 'Sop_gil'@'localhost' ;
+grant select, insert, update, delete on aerolinea.pago to 'Sop_gil'@'localhost' ;
+grant select, insert, update, delete on aerolinea.pais to 'Sop_gil'@'localhost' ;
+grant select, insert, update, delete on aerolinea.pasajero to 'Sop_gil'@'localhost' ;
+grant select, insert, update, delete on aerolinea.reservacion to 'Sop_gil'@'localhost' ;
+grant select, insert, update, delete on aerolinea.salida to 'Sop_gil'@'localhost' ;
+grant select, insert, update, delete on aerolinea.tarifa to 'Sop_gil'@'localhost' ;
+grant select, insert, update, delete on aerolinea.tipoequipaje to 'Sop_gil'@'localhost' ;
+grant select, insert, update, delete on aerolinea.visa to 'Sop_gil'@'localhost' ;
+grant select, insert, update, delete on aerolinea.vuelo to 'Sop_gil'@'localhost' ;
+grant select, insert, update, delete on aerolinea.usuariospasajero to 'Sop_gil'@'localhost' ;
+grant select, insert, update, delete on aerolinea.usuarios to 'Sop_gil'@'localhost' ;
+
+-- contabilidad
+grant select, insert, update, delete on aerolinea.metodopago to 'Cont_Fermin'@'localhost';
+grant select, insert, update, delete on aerolinea.millas to 'Cont_Fermin'@'localhost';
+grant select, insert, update, delete on aerolinea.pago to 'Cont_Fermin'@'localhost';
+grant select, insert, update, delete on aerolinea.tarifa to 'Cont_Fermin'@'localhost';
+
+-- Inventario
+grant select, insert, update, delete on aerolinea.equipaje to 'Inven_Enzo'@'localhost' ;
+grant select, insert, update, delete on aerolinea.aeronave to 'Inven_Enzo'@'localhost' ;
+
+-- RRHH
+grant select, insert, update, delete on aerolinea.cargo to 'RRHH_Mario'@'localhost' ;
+grant select, insert, update, delete on aerolinea.ciudad to 'RRHH_Mario'@'localhost' ;
+grant select, insert, update, delete on aerolinea.direcciones to 'RRHH_Mario'@'localhost' ;
+grant select, insert, update, delete on aerolinea.empleado to 'RRHH_Mario'@'localhost' ;
+grant select, insert, update, delete on aerolinea.estado to 'RRHH_Mario'@'localhost' ;
+grant select, insert, update, delete on aerolinea.pais to 'RRHH_Mario'@'localhost' ;
+grant select, insert, update, delete on aerolinea.piloto to 'RRHH_Mario'@'localhost' ;
+grant select, insert, update, delete on aerolinea.sobrecargo to 'RRHH_Mario'@'localhost' ;
+grant select, insert, update, delete on aerolinea.visa to 'RRHH_Mario'@'localhost' ;
+
+-- Ingeniero
+grant select, insert, update, delete on aerolinea.clases to 'ING_Jorge'@'localhost' ;
+grant select, insert, update, delete on aerolinea.aeronave to 'ING_Jorge'@'localhost' ;
+
+-- Planificador
+grant select, insert, update, delete on aerolinea.ciudad to 'PLAN_Tono'@'localhost' ;
+grant select, insert, update, delete on aerolinea.destino to 'PLAN_Tono'@'localhost' ;
+grant select, insert, update, delete on aerolinea.direcciones to 'PLAN_Tono'@'localhost' ;
+grant select, insert, update, delete on aerolinea.escala to 'PLAN_Tono'@'localhost' ;
+grant select, insert, update, delete on aerolinea.estado to 'PLAN_Tono'@'localhost' ;
+grant select, insert, update, delete on aerolinea.origen to 'PLAN_Tono'@'localhost' ;
+grant select, insert, update, delete on aerolinea.pais to 'PLAN_Tono'@'localhost' ;
+grant select, insert, update, delete on aerolinea.regreso to 'PLAN_Tono'@'localhost' ;
+grant select, insert, update, delete on aerolinea.salida to 'PLAN_Tono'@'localhost' ;
+grant select, insert, update, delete on aerolinea.vuelo to 'PLAN_Tono'@'localhost' ;
+
+-- cliente
+grant select, insert, update, delete on aerolinea.asiento to Cliente;
+grant select, insert, update, delete on aerolinea.boleto to Cliente;
+grant select, insert, update, delete on aerolinea.ciudad to Cliente;
+grant select, insert, update, delete on aerolinea.clases to Cliente;
+grant select, insert, update, delete on aerolinea.destino to Cliente;
+grant select, insert, update, delete on aerolinea.direcciones to Cliente;
+grant select, insert, update, delete on aerolinea.estado to Cliente;
+grant select, insert, update, delete on aerolinea.fecha to Cliente;
+grant select, insert, update, delete on aerolinea.itinerario to Cliente;
+grant select, insert, update, delete on aerolinea.metodopago to Cliente;
+grant select, insert, update, delete on aerolinea.millas to Cliente;
+grant select, insert, update, delete on aerolinea.origen to Cliente;
+grant select, insert, update, delete on aerolinea.pago to Cliente;
+grant select, insert, update, delete on aerolinea.pais to Cliente;
+grant select, insert, update, delete on aerolinea.pasajero to Cliente;
+grant select, insert, update, delete on aerolinea.reservacion to Cliente;
+grant select, insert, update, delete on aerolinea.salida to Cliente;
+grant select, insert, update, delete on aerolinea.tarifa to Cliente;
+grant select, insert, update, delete on aerolinea.tipoequipaje to Cliente;
+grant select, insert, update, delete on aerolinea.visa to Cliente;
+grant select, insert, update, delete on aerolinea.vuelo to Cliente;
+grant select, insert, update, delete on aerolinea.usuariospasajero to Cliente;
+
 
 -- VINCULAR USUARIOS A ROLES
 grant SysAdmin TO 'Admin_Gil'@'localhost';
